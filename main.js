84f683db4b66e9c45b37d69788f6ed33c3086d71
DevDivyansh <576divyanshsharma@gmail.com>
2026-03-21 01:21:47 +0530

    V.1.1.1 is ready need to implant logic for some problems

--- main.js
+++ main.js
@@ -1,52 +1,121 @@
-const targets = document.body.querySelectorAll("*")
+const targets = document.body.querySelectorAll("*");
 // console.log(targets)
 
-
 const properties = {
-    m : "margin",
-    p : "padding",
-    bg : "background-color",
-    clr : "color",
-    border : "border"
-}
+  /****Margin****/
+  m: "margin",
+  mt: "margin-top",
+  mb: "margin-bottom",
+  ml: "margin-left",
+  mr: "margin-right",
 
-const toAddPx = ["margin","padding"]
-targets.forEach((target)=>{
-    
-    target.classList.forEach((cls)=>{
-            if(cls.startsWith("do-")){
-                
-                const toDo = cls.split("-")
-                console.log(toDo)
-                let [doit , property , ...value] = [...toDo]
-                // if( toAddPx.includes(properties[property])){
-                //     value = value+"px"
-                // }
-                console.log(doit)
-                console.log(property)
-                console.log(value.join(" "))
-                console.log(value.join(" "))
-                // console.log(doit , property , value)
-                // console.log(`${properties[property]}:${value}`);
+  clr: "color",
 
-                // target.setAttribute("style",`${properties[property]}: ${value}`) This is overriding old styles okk
+  /****Padding****/
+  p: "padding",
+  pt: "padding-top",
+  pb: "padding-bottom",
+  pl: "padding-left",
+  pr: "padding-right",
 
-               target.style[properties[property]] = value.join(" ")
-            }
-    })
-})
+  /****background****/
+  bg: "background-color",
+  // opacity : "opacity",
+  bgImg: "background-image",
+
+  /****border****/
+  // border : "border",
+  brdStyle: "border-style",
+  brdWidth: "border-width",
+  brdClr: "border-color",
+  brdRadius: "border-radius",
+
+  boxSizing: "box-sizing",
+
+  minWidth: "min-width",
+  maxWidth: "max-width",
+  minHeight: "min-height",
+  maxHeight: "max-height",
+
+  /****outline****/
+  outlineStyle: "outline-style",
+  outlineClr: "outline-color",
+  outlineWidth: "outline-width",
+
+  /****text****/
+  textAlign: "text-align",
+  verticalAlign: "vertical-align",
+  textDecorationLine: "text-decoration-line",
+  textDecorationColor: "text-decoration-color",
+  textDecorationStyle: "text-decoration-style",
+  textTransform: "text-transform",
+  textSpacing : "text-spacing",
+  textShadow : "text-shadow",
+  
+  /* ****Fonts**** */
+
+  fontStyle : "font-style",
+  fontSize : "font-size",
+  fontVariant : "font-variant",
+  fontWeight : "font-weight",
+  fontStretch : "font-stretch",
 
 
-     // *******Basic POC i have applied but it will give only direct child*********
+
+
+
+
+};
+
+const toAddPx = ["margin", "padding"];
+targets.forEach((target) => {
+  target.classList.forEach((cls) => {
+    if (cls.startsWith("do-")) {
+      const toDo = cls.trim().split("-");
+      console.log(toDo);
+      let [_, property, ...value] = toDo;
+      // if( toAddPx.includes(properties[property])){
+      //     value = value+"px"
+      // }
+      console.log(property);
+      console.log(value.join(" "));
+      // console.log(doit , property , value)
+      // console.log(`${properties[property]}:${value}`);
+
+      // target.setAttribute("style",`${properties[property]}: ${value}`) This is overriding old styles okk
+
+      target.style[properties[property] || property] = Array.isArray(value)
+        ? value.join(" ")
+        : value;
+    }
+  });
+});
+
+// *******Basic POC i have applied but it will give only direct child*********
 // const elements = document.body.childNodes
 
 // elements.forEach((node)=>{
-    
+
 //     if( node.nodeType === 1){
 //         console.log(node)
-//     } 
+//     }
 // })
 
 // document.body.setAttribute("style","min-height : 100vh")
 
 // element.setAttribute("style","margin: 400 200")
+
+/* Improvement's i need to do in this project are 
+first i should implement to use rgb(0, 0, 255) currently unable to do so due to blank space separation " "
+
+2. I need to work on fonts class as it is not able to implement right now
+
+3. Need to implant logic for handling element states like : 
+The four link states are:
+
+:link - a normal, unvisited link
+:visited - a link the user has visited
+:hover - a link when the user mouses over it
+:active - a link the moment it is clicked
+
+*/
